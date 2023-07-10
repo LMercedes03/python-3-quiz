121afb841791d9420cd477ba7b932b66e24264d8
Leonard Mercedes <100613411+LMercedes03@users.noreply.github.com>
2023-07-10 10:56:22 -0400

    Complete Python III quiz

--- name_checker.py
+++ name_checker.py
@@ -1,7 +1,22 @@
 from ValidationException import ValidationException
 
-def validate_file(file_path):
-    pass #TODO: Only add code inside this function.
+
+def validate_file(filename):
+    numbers = "0123456789"
+
+    with open(filename, "r") as in_stream:
+        for line_number, line in enumerate(in_stream, start=1):
+            if line_number == 1:
+                continue  # Skip the header line
+
+            firstname, lastname, username, posts, likes, gamerewards = line.strip().split(',')
+            for char in firstname:
+                if char in numbers:
+                    try:
+                        firstname = int(firstname)
+                    except ValueError:
+                        # If numbers in firstname, raise a ValidationException
+                        raise ValidationException(f"Invalid first name: {firstname}")
 
 
 def test():
@@ -9,5 +24,6 @@ def test():
         validate_file("users.txt")
     except ValidationException as ve:
         print(ve)
+
 
 test()
